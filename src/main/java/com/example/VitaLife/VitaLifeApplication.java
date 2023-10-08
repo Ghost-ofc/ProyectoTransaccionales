aaaf440d266eb1fd2bbc025cc591a9117c31a095
Juan Gutierrez <81880988+Ghost-ofc@users.noreply.github.com>
2023-10-07 20:52:58 -0500

    Inicio del proyecto

---
+++ src/main/java/com/example/VitaLife/VitaLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.VitaLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VitaLife/VitaLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VitaLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
