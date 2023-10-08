bcdbe029b7bd26af1522f2de0007df6aef5622bb
Juan Gutierrez <81880988+Ghost-ofc@users.noreply.github.com>
2023-10-07 20:36:50 -0500

    Delete src directory

--- src/main/java/com/example/VitaLife10/Repository/TituloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.VitaLife10.Repository;
-
-import com.example.VitaLife10.entity.Titulo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TituloRepository extends JpaRepository<Titulo, Long> {
-}
