b0ad58a252a0f2e5cfb8521fb07a198d7f3672e9
Angelo Sanchez <jsanchezs17@upao.edu.pe>
2023-10-08 20:27:56 -0500

    Historia De usuario Numero 20

--- src/main/java/com/example/VitaLife10/Controller/UsuarioController.java
+++ src/main/java/com/example/VitaLife10/Controller/UsuarioController.java
@@ -1,14 +1,13 @@
 package com.example.VitaLife10.Controller;
 
 import com.example.VitaLife10.Service.UsuarioService;
-import com.example.VitaLife10.entity.ApiResponse;
+import com.example.VitaLife10.Metodos.ApiResponse;
 import com.example.VitaLife10.entity.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/usuarios")
